cd5985d061d155699054ca1e08c90c043ad18ffd
Jovie <jovelyn.quijano@clicktekconsulting.com>
2025-11-26 12:37:03 +0800

    fixed config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  roots: ["<rootDir>/tests/contract", "<rootDir>/tests/guardrail"],
-  testMatch: ["**/*.test.js"],
-  collectCoverage: true,
-  coverageDirectory: "coverage",
+  preset: 'ts-jest', // enables TS support
+  testEnvironment: 'node', // good for backend/API tests
+  transform: {
+    '^.+\\.(ts|js)$': 'ts-jest', // handle both JS & TS
+  },
+  testMatch: [
+    '**/__tests__/**/*.+(ts|js|tsx|jsx)',
+    '**/?(*.)+(spec|test).+(ts|js|tsx|jsx)'
+  ],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  verbose: true, // nice to see test results in detail
 };
